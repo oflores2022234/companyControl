881672478bdbd3698cd3505234149733702d8462
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-02-22 00:07:10 -0600

    we write the code to the model of Users

---
+++ src/users/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    nombre: {
+      type: String,
+      required: [true, "El nombre es obligatorio"],
+    },
+    correo: {
+      type: String,
+      required: [true, "El correo es obligarorio"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "La contraseña es obligaroria"],
+    },
+    role: {
+      type: String,
+      default:"ADMIN_ROLE"
+    },
+    estado: {
+      type: Boolean,
+      default: true,
+    }
+  });
+
+  UserSchema.methods.toJSON = function(){
+    const { __v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+  }
+  
+  export default mongoose.model('User', UserSchema);
