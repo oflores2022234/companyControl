ff66262c1ff832ab246d40a11f1875f58cfa51d6
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-02-22 00:00:27 -0600

    configure and inicialize server for the aplication

---
+++ configs/server.js
@@ -0,0 +1,45 @@
+'use strict'
+
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnection } from './mongo.js';
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        //this.usuarioPath = '/coffeApi/v1/users'
+        //this.authPath = '/coffeApi/v1/auth'
+
+        this.middlewares();
+        this.conectarDB();
+        //this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    /*routes(){
+        this.app.use(this.usuarioPath, userRoutes);
+        this.app.use(this.authPath, authRoutes)
+    }*/
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Server running on port ', this.port);
+        });
+    }
+}
+
+export default Server;
